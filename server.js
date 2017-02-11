0965a4ce62738e1d73afeed7b8599d5333b9f291
Harry N <harry.nguyentrongdat@gmail.com>
2017-02-11 22:32:17 +0700

    refactor with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -18,24 +20,28 @@ app.get('/todos', function(req, res) {
 
 // GET /todo/:id
 app.get('/todo/:id', function(req, res) {
-	var id = req.params.id;
-	for (var i = 0; i < todos.length; i++) {
-		if (todos[i].id == id) {
-			res.json(todos[i]);
-			return;
-		}
+	var id = parseInt(req.params.id);
+	var matched = _.findWhere(todos, id);
+	
+	if (matched) {
+		res.json(matched);
+	} else {
+		res.status(404).send();
 	}
-	res.status(404).send();
 });
 
 // POST /todos
 app.post('/todos', function(req, res) {
-	var body = req.body;
-	if (typeof body.description !== 'undefined') {
-		body.id = index;
-		index++;
-		todos.push(body);
+	var body = _.pick(req.body, "description", "completed");
+
+	if (!_.isBoolean(body.completed) || (!_.isString(body.description)) || body.description.trim().length === 0) {
+		return res.status(400).send();
 	}
+
+	body.description = body.description.trim();
+	body.id = index++;
+	todos.push(body);
+
 	res.json(body);
 });
 
