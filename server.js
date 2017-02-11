2b3887b25f8fff2915145911a481764352157660
Harry N <harry.nguyentrongdat@gmail.com>
2017-02-11 21:26:55 +0700

    add POST /todos

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Shopping',
-	completed: false
-}, {
-	id: 3,
-	description: 'Feed',
-	completed: true
-}];
+var todos = [];
+var index = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('TODO root');
 });
 
+// GET /todos
 app.get('/todos', function(req, res) {
 	res.json(todos);
 });
 
+// GET /todo/:id
 app.get('/todo/:id', function(req, res) {
 	var id = req.params.id;
 	for (var i = 0; i < todos.length; i++) {
@@ -34,6 +28,16 @@ app.get('/todo/:id', function(req, res) {
 	res.status(404).send();
 });
 
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+	if (typeof body.description !== 'undefined') {
+		body.id = index;
+		index++;
+		todos.push(body);
+	}
+	res.json(body);
+});
 
 app.listen(PORT, function() {
 	console.log('Listening to port ' + PORT);
