24a8baccefe8de6ce8cbdfbf07d8ab3fd21a1e40
Harry N <harry.nguyentrongdat@gmail.com>
2017-02-12 21:22:37 +0700

    add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -53,6 +54,12 @@ app.get('/todo/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, "description", "completed");
 
+	db.todo.create(body).then(function(todo) {
+		res.json(todo.toJSON());
+	}).catch(function(e) {
+		res.status(400).json(e);
+	});
+	/*
 	if (!_.isBoolean(body.completed) || (!_.isString(body.description)) || body.description.trim().length === 0) {
 		return res.status(400).send();
 	}
@@ -62,6 +69,7 @@ app.post('/todos', function(req, res) {
 	todos.push(body);
 
 	res.json(body);
+	*/
 });
 
 // DELETE /todo/:id
@@ -110,6 +118,8 @@ app.put('/todo/:id', function(req, res) {
 	res.json(matched);
 });
 
-app.listen(PORT, function() {
-	console.log('Listening to port ' + PORT);
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Listening to port ' + PORT);
+	});
 });
